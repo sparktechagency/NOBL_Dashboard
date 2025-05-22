8379ee90669e30fe9c93feaa99b6e1e2b0cb766e
arifbiswas <mxgangstar310@gmail.com>
2025-05-22 22:14:54 +0600

    added

--- src/pages/PhotoLibrary.tsx
+++ src/pages/PhotoLibrary.tsx
@@ -1,11 +1,114 @@
-import React, { useState } from "react";
-import CategoryCard from "../component/PhotoLibary/CategoryCard";
-import PhotoAddModal from "../component/PhotoLibary/PhotoAddModal";
-import { Select } from "antd";
-import { Option } from "antd/es/mentions";
+import { Button, Card, Pagination, Select } from "antd";
+import { useState } from "react";
+
 import { DownOutlined } from "@ant-design/icons";
+import Swal from "sweetalert2";
+import img1 from "../assets/Images/photoLibray/Rectangle 1308.png";
+import img2 from "../assets/Images/photoLibray/Rectangle 1309.png";
+import img3 from "../assets/Images/photoLibray/Rectangle 1310.png";
+import img4 from "../assets/Images/photoLibray/Rectangle 1311.png";
+import img5 from "../assets/Images/photoLibray/Rectangle 1312.png";
+import img6 from "../assets/Images/photoLibray/Rectangle 1313.png";
+import img7 from "../assets/Images/photoLibray/Rectangle 1314.png";
+import img8 from "../assets/Images/photoLibray/Rectangle 1315.png";
+import img9 from "../assets/Images/photoLibray/Rectangle 1316.png";
+import img10 from "../assets/Images/photoLibray/Rectangle 1317.png";
+import PhotoAddModal from "../component/PhotoLibary/PhotoAddModal";
+
+const photoLibraryData = [
+  {
+    id: 1,
+    category: "Categoty 1",
+    Image: img1,
+  },
+  {
+    id: 2,
+    category: "Categoty 2",
+    Image: img2,
+  },
+  {
+    id: 3,
+    category: "Categoty 3",
+    Image: img3,
+  },
+  {
+    id: 4,
+    category: "Categoty 4",
+    Image: img4,
+  },
+  {
+    id: 5,
+    category: "Categoty 5",
+    Image: img5,
+  },
+  {
+    id: 6,
+    category: "Categoty 6",
+    Image: img6,
+  },
+  {
+    id: 7,
+    category: "Categoty 7",
+    Image: img7,
+  },
+  {
+    id: 8,
+    category: "Categoty 8",
+    Image: img8,
+  },
+  {
+    id: 9,
+    category: "Categoty 9",
+    Image: img9,
+  },
+  {
+    id: 10,
+    category: "Categoty 10",
+    Image: img10,
+  },
+  {
+    id: 11,
+    category: "Categoty 10",
+    Image: img10,
+  },
+  {
+    id: 12,
+    category: "Categoty 10",
+    Image: img10,
+  },
+];
 
 const PhotoLibrary = () => {
+  // show delete change photo model state
+  const [imageMadel, setImageMadel] = useState(false);
+  const [imageID, setimageID] = useState<number | null>(null);
+
+  // hendel modal delete and chage open and close
+  const hendelClick = (id) => {
+    setImageMadel(!imageMadel);
+    setimageID(id);
+  };
+
+  const hendelDelete = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
     setIsModalOpen(true);
@@ -56,7 +159,132 @@ const PhotoLibrary = () => {
         </div>
       </div>
       {/* photo Card */}
-      <CategoryCard />
+      <div className="relative flex-col justify-between items-end">
+        <div className="grid grid-cols-5 gap-5">
+          {photoLibraryData.map((item) => (
+            <Card
+              style={{ width: 284, margin: 0 }}
+              bodyStyle={{
+                padding: 0,
+                margin: 0,
+              }}
+            >
+              <div className="relative">
+                <img
+                  alt="example"
+                  className="w-[284px] h-[156] rounded-md"
+                  src={item.Image}
+                />
+                <svg
+                  onClick={() => hendelClick(item.id)}
+                  width="35"
+                  height="35"
+                  viewBox="0 0 35 35"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="absolute top-2 right-2"
+                >
+                  <rect
+                    x="0.5"
+                    y="0.5"
+                    width="34"
+                    height="34"
+                    rx="17"
+                    fill="white"
+                    fill-opacity="0.24"
+                  />
+                  <rect
+                    x="0.5"
+                    y="0.5"
+                    width="34"
+                    height="34"
+                    rx="17"
+                    stroke="white"
+                  />
+                  <path
+                    d="M17.5 10.9483C18.4522 10.9483 19.2242 10.1764 19.2242 9.22414C19.2242 8.27192 18.4522 7.5 17.5 7.5C16.5478 7.5 15.7759 8.27192 15.7759 9.22414C15.7759 10.1764 16.5478 10.9483 17.5 10.9483Z"
+                    fill="white"
+                    stroke="white"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M17.5 19.2242C18.4522 19.2242 19.2242 18.4522 19.2242 17.5C19.2242 16.5478 18.4522 15.7759 17.5 15.7759C16.5478 15.7759 15.7759 16.5478 15.7759 17.5C15.7759 18.4522 16.5478 19.2242 17.5 19.2242Z"
+                    fill="white"
+                    stroke="white"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M17.5 27.5C18.4522 27.5 19.2242 26.7281 19.2242 25.7759C19.2242 24.8237 18.4522 24.0518 17.5 24.0518C16.5478 24.0518 15.7759 24.8237 15.7759 25.7759C15.7759 26.7281 16.5478 27.5 17.5 27.5Z"
+                    fill="white"
+                    stroke="white"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+
+                <div className="bg-white py-1 px-2  absolute bottom-2 left-2 rounded-md">
+                  <p className="text-sm font-medium font-popping ">
+                    Category 2
+                  </p>
+                </div>
+              </div>
+              {imageMadel && imageID == item.id && (
+                <div className="bg-white  absolute bottom-11 right-4 cursor-pointer shadow-lg">
+                  {/* change photo */}
+                  <Button className="flex  justify-start items-center  w-full  border-none px-3 py-1">
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M20 6.8399C20.0004 8.19129 19.6008 9.51255 18.8515 10.6371C18.1021 11.7616 17.0367 12.6391 15.7895 13.1589V13.1567C15.7895 10.7834 14.8468 8.50718 13.1688 6.82895C11.4909 5.15071 9.2151 4.20788 6.84211 4.20788H6.84C7.44927 2.74841 8.54504 1.54498 9.94106 0.802111C11.3371 0.0592422 12.9472 -0.177224 14.4978 0.132902C16.0485 0.443027 17.4439 1.28061 18.4469 2.50329C19.45 3.72598 19.9987 5.25832 20 6.8399ZM4.73684 0.523056C3.62014 0.523056 2.54918 0.966737 1.75955 1.7565C0.969923 2.54626 0.526316 3.6174 0.526316 4.73429V6.3135H2.63158V4.73429C2.63158 4.17584 2.85338 3.64027 3.2482 3.24539C3.64301 2.85051 4.17849 2.62867 4.73684 2.62867H6.31579V0.523056H4.73684ZM17.3684 13.6832V15.2624C17.3684 15.8208 17.1466 16.3564 16.7518 16.7513C16.357 17.1461 15.8215 17.368 15.2632 17.368H13.6842V19.4736H15.2632C16.3799 19.4736 17.4508 19.0299 18.2404 18.2402C19.0301 17.4504 19.4737 16.3793 19.4737 15.2624V13.6832H17.3684ZM6.84211 20C8.65674 20 10.3971 19.279 11.6802 17.9957C12.9633 16.7123 13.6842 14.9717 13.6842 13.1567C13.6842 11.3418 12.9633 9.6012 11.6802 8.31784C10.3971 7.03448 8.65674 6.3135 6.84211 6.3135C5.02747 6.3135 3.28715 7.03448 2.00401 8.31784C0.720862 9.6012 0 11.3418 0 13.1567C0 14.9717 0.720862 16.7123 2.00401 17.9957C3.28715 19.279 5.02747 20 6.84211 20ZM6.84211 10.5247L9.47368 13.1567L6.84211 15.7888L4.21053 13.1567L6.84211 10.5247Z"
+                        fill="#4B5320"
+                      />
+                    </svg>
+                    <p className="font-medium text-lg font-roboto text-[#4B5320] ">
+                      Change photo
+                    </p>
+                  </Button>
+                  {/* change photo */}
+                  <Button
+                    onClick={hendelDelete}
+                    className="flex justify-start  items-center  w-full bg-[#FFEBEB] border-none px-3 py-1"
+                  >
+                    <svg
+                      width="16"
+                      height="20"
+                      viewBox="0 0 16 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M16 1.11111H12L10.8571 0H5.14286L4 1.11111H0V3.33333H16M1.14286 17.7778C1.14286 18.3671 1.38367 18.9324 1.81233 19.3491C2.24098 19.7659 2.82236 20 3.42857 20H12.5714C13.1776 20 13.759 19.7659 14.1877 19.3491C14.6163 18.9324 14.8571 18.3671 14.8571 17.7778V4.44444H1.14286V17.7778Z"
+                        fill="#FF0000"
+                      />
+                    </svg>
+
+                    <p className="font-medium text-lg font-roboto text-[#FF0000] ">
+                      Delete photo
+                    </p>
+                  </Button>
+                </div>
+              )}
+            </Card>
+          ))}
+        </div>
+        {/* Pagination */}
+        <div className="absolute -bottom-[150px] right-0  ">
+          <Pagination defaultCurrent={1} total={50} />
+        </div>
+      </div>
       {/* add new photo modal*/}
       <PhotoAddModal
         isModalOpen={isModalOpen}
--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,15 +1,201 @@
+import { Image, Popconfirm, Select, Space, Table, message } from "antd";
+
 import { DownOutlined } from "@ant-design/icons";
-import { Divider, Select } from "antd";
 import { Option } from "antd/es/mentions";
-import React, { useState } from "react";
-import VideosTable from "../component/videos/VideosTable";
+import { useState } from "react";
+import img2 from "../assets/Images/videos/Rectangle 8 (1).png";
+import img3 from "../assets/Images/videos/Rectangle 8 (2).png";
+import img4 from "../assets/Images/videos/Rectangle 8 (3).png";
+import img5 from "../assets/Images/videos/Rectangle 8 (4).png";
+import img6 from "../assets/Images/videos/Rectangle 8 (5).png";
+import img7 from "../assets/Images/videos/Rectangle 8 (6).png";
+import img1 from "../assets/Images/videos/Rectangle 8.png";
 import VideoModal from "../component/videos/VideoModal";
 
 const Videos = () => {
+  // show view modal
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditOpenModal, setIsEditOpenModal] = useState(false); //edit modal open stat
+
   const showModal = () => {
     setIsModalOpen(true);
   };
+  // edit modal
+
+  const showEditModal = () => {
+    setIsEditOpenModal(true);
+  };
+
+  // delete modal
+  const confirm = (e) => {
+    console.log(e);
+    message.success("Click on Yes");
+  };
+
+  const cancel = (e) => {
+    console.log(e);
+    message.error("Click on No");
+  };
+  // delete modal end
+
+  const columns = [
+    {
+      title: "Sl. no.",
+      dataIndex: "serial",
+      key: "serial",
+      align: "center",
+    },
+    {
+      title: "Video",
+      dataIndex: "video",
+      key: "video",
+      render: (video: any) => (
+        <Space>
+          <Image width={40} src={video.thumbnail} alt="thumbnail" />
+          <span>{video.title}</span>
+        </Space>
+      ),
+    },
+    {
+      title: "Category",
+      dataIndex: "category",
+      key: "category",
+      align: "center",
+    },
+    {
+      title: "Action",
+      key: "action",
+      align: "center",
+      render: (_, record) => (
+        <Space size="middle">
+          {/* view icon */}
+          <div onClick={showModal}>
+            <svg
+              width="37"
+              height="37"
+              viewBox="0 0 37 37"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="37" height="37" rx="5" fill="#FFF3EB" />
+              <path
+                d="M18 15.8C17.132 15.8 16.2996 16.1371 15.6858 16.7373C15.0721 17.3374 14.7273 18.1513 14.7273 19C14.7273 19.8487 15.0721 20.6626 15.6858 21.2627C16.2996 21.8629 17.132 22.2 18 22.2C18.868 22.2 19.7004 21.8629 20.3142 21.2627C20.9279 20.6626 21.2727 19.8487 21.2727 19C21.2727 18.1513 20.9279 17.3374 20.3142 16.7373C19.7004 16.1371 18.868 15.8 18 15.8ZM18 24.3333C16.5534 24.3333 15.166 23.7714 14.1431 22.7712C13.1201 21.771 12.5455 20.4145 12.5455 19C12.5455 17.5855 13.1201 16.229 14.1431 15.2288C15.166 14.2286 16.5534 13.6667 18 13.6667C19.4466 13.6667 20.834 14.2286 21.8569 15.2288C22.8799 16.229 23.4545 17.5855 23.4545 19C23.4545 20.4145 22.8799 21.771 21.8569 22.7712C20.834 23.7714 19.4466 24.3333 18 24.3333ZM18 11C12.5455 11 7.88727 14.3173 6 19C7.88727 23.6827 12.5455 27 18 27C23.4545 27 28.1127 23.6827 30 19C28.1127 14.3173 23.4545 11 18 11Z"
+                fill="#F96D10"
+              />
+            </svg>
+          </div>
+
+          <div onClick={showEditModal}>
+            <svg
+              width="37"
+              height="37"
+              viewBox="0 0 37 37"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="37" height="37" rx="5" fill="#E4FFEB" />
+              <path
+                d="M21 13.1716L24 16.1716M19 27.1716H27M11 23.1716L10 27.1716L14 26.1716L25.586 14.5856C25.9609 14.2105 26.1716 13.7019 26.1716 13.1716C26.1716 12.6412 25.9609 12.1326 25.586 11.7576L25.414 11.5856C25.0389 11.2106 24.5303 11 24 11C23.4697 11 22.9611 11.2106 22.586 11.5856L11 23.1716Z"
+                stroke="#28A745"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+          <div>
+            <Popconfirm
+              title="Are you sure to delete this video?"
+              onConfirm={confirm}
+              onCancel={cancel}
+              okText="Yes"
+              cancelText="No"
+            >
+              <svg
+                width="37"
+                height="37"
+                viewBox="0 0 37 37"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect width="37" height="37" rx="5" fill="#FFE6E6" />
+                <path
+                  d="M23 16V26H15V16H23ZM21.5 10H16.5L15.5 11H12V13H26V11H22.5L21.5 10ZM25 14H13V26C13 27.1 13.9 28 15 28H23C24.1 28 25 27.1 25 26V14Z"
+                  fill="#FF0000"
+                />
+              </svg>
+            </Popconfirm>
+          </div>
+        </Space>
+      ),
+    },
+  ];
+
+  const data = [
+    {
+      key: "1",
+      serial: "001",
+      video: {
+        thumbnail: img1,
+        title: "Training Video Part 1",
+      },
+      category: "Welcome to NOBL",
+    },
+    {
+      key: "2",
+      serial: "002",
+      video: {
+        thumbnail: img2,
+        title: "Training Video Part 2",
+      },
+      category: "Amply Value",
+    },
+    {
+      key: "3",
+      serial: "003",
+      video: {
+        thumbnail: img3,
+        title: "Training Video Part 3",
+      },
+      category: "Introduction",
+    },
+    {
+      key: "4",
+      serial: "004",
+      video: {
+        thumbnail: img4,
+        title: "Training Video Part 4",
+      },
+      category: "Key to success in this industry",
+    },
+    {
+      key: "5",
+      serial: "005",
+      video: {
+        thumbnail: img5,
+        title: "Training Video Part 5",
+      },
+      category: "Introduction",
+    },
+    {
+      key: "6",
+      serial: "006",
+      video: {
+        thumbnail: img6,
+        title: "Training Video Part 6",
+      },
+      category: "Transitioning",
+    },
+    {
+      key: "7",
+      serial: "007",
+      video: {
+        thumbnail: img7,
+        title: "Training Video Part 7",
+      },
+      category: "Building Value",
+    },
+  ];
 
   // categories
   const categories = [
@@ -106,7 +292,13 @@ const Videos = () => {
         </div>
       </div>
       {/* table */}
-      <VideosTable />
+      <Table
+        columns={columns}
+        rowClassName={() => "table-row-gap"}
+        className="custom-ant-table"
+        dataSource={data}
+        pagination={true}
+      />
       {/* video modal */}
       <VideoModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </div>
